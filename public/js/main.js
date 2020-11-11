0c6ee0dbeabaa617d305308c88dc88354bf80e60
Sujata Gunale <tidbitsjs@gmail.com>
2020-11-11 22:53:09 +0530

    chat layout

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+const userSelect = document.querySelector("#user-select");
+const users = document.querySelector("#users");
+var set = false;
+
+userSelect.addEventListener("click", () => {
+  console.log(userSelect.dataset.set);
+  if (set === false) {
+    users.style.display = "block";
+    set = true;
+  } else {
+    users.style.display = "none";
+    set = false;
+  }
+});
